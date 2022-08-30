e5f6460edb40f161087c1eb951b7af24b577ea64
Abinaya890 <abinayachandrakumar27@gmail.com>
2022-08-30 20:18:39 +0530

    CartDetails

--- force-app/main/default/aura/CartInfoComponent/CartInfoComponentController.js
+++ force-app/main/default/aura/CartInfoComponent/CartInfoComponentController.js
@@ -1,5 +1,18 @@
 ({
-	myAction : function(component, event, helper) {
+	goToCart : function(component, event, helper) {
+		var pageReference = component.find("navService");
+		var pageReferenceNav = {    
+			"type": "standard__component",
+			"attributes": {
+				"componentName": "c__CartDetails"    
+			},    
+			state: {
+				c__cartRecord : component.get("v.recordList")
+				
+			}
+		};
+		pageReference.navigate(pageReferenceNav);
+	
 		
 	}
 })
