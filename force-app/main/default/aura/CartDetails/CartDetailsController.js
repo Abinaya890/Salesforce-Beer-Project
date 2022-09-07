89372bd1604b542bc57f730d29d276681db25f02
Abinaya890 <abinayachandrakumar27@gmail.com>
2022-09-07 20:59:49 +0530

    address update

--- force-app/main/default/aura/CartDetails/CartDetailsController.js
+++ force-app/main/default/aura/CartDetails/CartDetailsController.js
@@ -1,6 +1,24 @@
 ({
+
+    
     doInit : function(component, event, helper) 
     {
+        component.find('recordCreator').getNewRecord(
+            'Address_Book__c',
+            null,
+            false,
+            $A.getCallback(function(){
+                var record = component.get('v.record');
+                var error = component.get('v.recordError');
+                if(error || (record === null)){
+                    console.log(' Error while creating the template ',error);
+                }else{
+                    console.log(' Successfuly Created');
+                    //alert('Templated Initiated');
+                }
+            })
+        );
+
         var pageReference = component.get('v.pageReference');
         
         var state = pageReference.state;
@@ -127,5 +145,34 @@
     doCheckout : function(component, event, helper) {
      component.set('v.isCheckout', true);
 
+    },
+
+    doSaveAddress : function(component, event, helper){
+        component.find('recordCreator').saveRecord(function(saveResult){
+            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                // record is saved successfully
+                var resultsToast = $A.get("e.force:showToast");
+                resultsToast.setParams({
+                    "title": "Success",
+                    "type" : "success",
+                    "message": "The record was saved."
+                    
+                });
+                resultsToast.fire();
+
+            } else if (saveResult.state === "INCOMPLETE") {
+                // handle the incomplete state
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                // handle the error state
+                console.log('Problem saving contact, error: ' + JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+        });
     }
+
+
+
+ 
 })
